bbac867821984c1c50b6336b0cbe1257e41ba6ba
Amir Hashmi <hashmiamir@live.com>
2026-07-08 18:55:38 +0530

    [FIX] Menu gaps, quiz, certificates, placeholders, statistics

--- assets/js/components.js
+++ assets/js/components.js
@@ -267,5 +267,11 @@
   inject('site-header-inject', HEADER);
   inject('site-footer-inject', FOOTER);
   document.head.insertAdjacentHTML('beforeend', ORG_SCHEMA);
+  
+  // Inject Quiz & Certificate Script
+  const quizScript = document.createElement('script');
+  quizScript.src = '/assets/js/quiz-certificate.js';
+  document.body.appendChild(quizScript);
+
   document.dispatchEvent(new CustomEvent('headerInjected'));
 })();
