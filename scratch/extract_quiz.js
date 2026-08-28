ee06d34ca16944299e47322da66c5d50be36656d
Amir Hashmi <hashmiamir@live.com>
2026-08-28 19:17:20 +0530

    chore: clean repository and prepare for new travel website build

--- scratch/extract_quiz.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('about/annual-reports/index.html', 'utf8');
-const quizStart = content.indexOf('<section id="quiz"');
-const quizEnd = content.indexOf('</section>', quizStart) + 10;
-if (quizStart !== -1) {
-    fs.writeFileSync('scratch/quiz_html.txt', content.substring(quizStart, quizEnd));
-}
